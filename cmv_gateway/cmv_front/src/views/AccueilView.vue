36af4f1c269e6d19651687d1f3910287458dba56
CyrilPonsan <cyril.ponsan@gmail.com>
2024-10-26 09:24:25 +0200

    dico ok

--- cmv_gateway/cmv_front/src/views/AccueilView.vue
+++ cmv_gateway/cmv_front/src/views/AccueilView.vue
@@ -1,11 +1,10 @@
-<template>
-  <div class="flex flex-col gap-y-8 text-xs">
-    <PageHeader :title="t('patients.home.title')" :description="t('patients.home.description')" />
-    <ListPatients :columns="columns" :patientsList="patientsList" />
-  </div>
-</template>
-
 <script setup lang="ts">
+/**
+ * Landing page de l'interface dédiée aux employés du service
+ * accueil.
+ * Cette vue affiche la liste des dossiers administratifs des
+ * patients de la clinique Montvert.
+ */
 import ListPatients from '@/components/ListPatients.vue'
 import PageHeader from '@/components/PageHeader.vue'
 import usePatientList from '@/composables/use-patients-list'
@@ -19,3 +18,10 @@ onMounted(() => {
   getPatientsList()
 })
 </script>
+
+<template>
+  <div class="flex flex-col gap-y-8 text-xs">
+    <PageHeader :title="t('patients.home.title')" :description="t('patients.home.description')" />
+    <ListPatients :columns="columns" :patientsList="patientsList" />
+  </div>
+</template>
