f0b1493f42f0c91880dd485ef78265d8ca21bb2e
CyrilPonsan <cyril.ponsan@gmail.com>
2025-01-18 00:03:51 +0100

    corrections images + liste des documents

---
+++ cmv_chambres/app/main.py
@@ -0,0 +1,60 @@
+from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from fastapi.exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler,
+)
+from fastapi.exceptions import RequestValidationError
+
+from app.routers import api
+from app.dependancies.db_session import get_db
+from app.sql import models
+from app.settings.database import engine
+from app.utils.logging_setup import LoggerSetup
+
+
+# Initialisation de la bdd
+models.Base.metadata.create_all(bind=engine)
+
+# Mise en place des logs
+logger = LoggerSetup()
+
+app = FastAPI()
+
+app.include_router(api.router)
+
+
+# Politiques CORS
+origins = []
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# handle app raised http exceptions
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request, exc):
+    print(f"OMG an HTTP error! {repr(exc)}")
+    if exc.status_code != 429:
+        logger.write_log(exc.detail, request)
+    return await http_exception_handler(request, exc)
+
+
+# handle validation exceptions
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    print(f"OMG! The client sent invalid data!: {exc}")
+    logger.write_valid(request, exc)
+    return await request_validation_exception_handler(request, exc)
+
+
+@app.get("/fixtures")
+def fixtures(db: Session = Depends(get_db)):
+    pass
--- cmv_gateway/cmv_front/src/components/documents/DocumentsHeader.vue
+++ cmv_gateway/cmv_front/src/components/documents/DocumentsHeader.vue
@@ -6,17 +6,26 @@ const { t } = useI18n()
 const emit = defineEmits<{
   (e: 'toggle-visible'): void
 }>()
+
+const { totalDocuments } = defineProps<{
+  totalDocuments: number
+}>()
 </script>
 
 <template>
-  <span class="flex flex-col xl:flex-row justify-between items-start xl:items-center gap-y-2 mb-4">
-    <h2 class="text-lg font-bold">
-      {{ t('components.documentsList.uploaded_documents') }}
-    </h2>
+  <span
+    class="w-full flex flex-col xl:flex-row justify-between items-start xl:items-center gap-y-2 pr-2"
+  >
+    <span class="flex gap-x-2 items-center">
+      <h2 class="text-lg font-bold">{{ t('components.documentsList.uploaded_documents') }}</h2>
+      <p>-</p>
+      <p class="text-sm font-normal">
+        {{ t('components.documentsList.total_documents', totalDocuments) }}
+      </p>
+    </span>
     <Button
       :label="t('components.documentsList.add_document')"
       icon="pi pi-paperclip"
-      outlined
       @click="emit('toggle-visible')"
     />
   </span>
