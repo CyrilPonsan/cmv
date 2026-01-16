a53549685a81d0c794bd16961d100a0a65cc9619
CyrilPonsan <91895995+CyrilPonsan@users.noreply.github.com>
2026-01-16 14:58:34 +0100

    fix redis 2

--- cmv_gateway/cmv_back/app/dependancies/redis.py
+++ cmv_gateway/cmv_back/app/dependancies/redis.py
@@ -1,5 +1,13 @@
 # Import du module Redis asynchrone
 from redis import asyncio as aioredis
 
+# Import de la configuration d'environnement
+from app.utils.config import ENVIRONMENT
 
-redis_client = aioredis.from_url("redis://localhost:6379", decode_responses=True)
+# Configuration du client Redis en fonction de l'environnement
+if ENVIRONMENT != "preprod":
+    # En développement, connexion à Redis sur localhost
+    redis_client = aioredis.from_url("redis://localhost:6379", decode_responses=True)
+else:
+    # En production, connexion à Redis via le service Docker
+    redis_client = aioredis.from_url("redis://redis:6379", decode_responses=True)
